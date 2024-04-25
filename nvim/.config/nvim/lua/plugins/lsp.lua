2b8d26bab6d972e3bf300495af7ec4dd25810d89
Mayrom <nhruo123@gmail.com>
2024-04-26 00:59:55 +0300

    added code action in visual mode

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -208,7 +208,7 @@ return {
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', {'n', 'v'})
 
           -- Opens a popup that displays documentation about the word under your cursor
           --  See `:help K` for why this keymap
