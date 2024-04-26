5e4c055a59cd02bae9bc4738a99a6d4d6fa0002c
Mayrom <nhruo123@gmail.com>
2024-04-27 00:29:42 +0300

    added auto fmt for rust

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -143,7 +143,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'black', 'isort', 'pylint'},
-        --
+        rust = {'rustfmt'}
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
