9be406758c9cdc4c15bb1110238fdc9b9f2f1af0
Mayrom Rabinovich <nhruo123@gmail.com>
2024-07-27 14:16:05 +0300

    made neotest lazy

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-neotest/neotest',
+  event = 'VeryLazy',
   dependencies = {
     'nvim-neotest/nvim-nio',
     'nvim-lua/plenary.nvim',
@@ -7,7 +8,6 @@ return {
     'nvim-treesitter/nvim-treesitter',
     'nvim-neotest/neotest-python',
   },
-
   config = function()
     require('neotest').setup {
       adapters = {
@@ -16,14 +16,28 @@ return {
         },
       },
     }
-
-    vim.keymap.set('n', '<leader>rt', require('neotest').run.run, { desc = 'Run nearest test' })
-    vim.keymap.set('n', '<leader>rT', function()
-      require('neotest').run.run(vim.fn.expand '%')
-    end, { desc = 'Run all tests in current file' })
-
-    vim.keymap.set('n', '<leader>rd', function()
-      require('neotest').run.run { strategy = 'dap' }
-    end, { desc = 'Debug the nearest test' })
   end,
+  keys = {
+    {
+      '<leader>rt',
+      function()
+        require('neotest').run.run()
+      end,
+      desc = 'Run nearest test',
+    },
+    {
+      '<leader>rT',
+      function()
+        require('neotest').run.run(vim.fn.expand '%')
+      end,
+      desc = 'Run all tests in current file',
+    },
+    {
+      '<leader>rd',
+      function()
+        require('neotest').run.run { strategy = 'dap' }
+      end,
+      desc = 'Debug the nearest test',
+    },
+  },
 }
