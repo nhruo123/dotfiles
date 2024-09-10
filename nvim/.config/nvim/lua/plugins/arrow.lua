b66c523961a5c8485d5f0b380844755682412047
Mayrom Rabinovich <mayrom@blockaid.co>
2024-09-10 13:43:35 +0300

    remove arrow

--- nvim/.config/nvim/lua/plugins/arrow.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'otavioschwanck/arrow.nvim',
-  opts = {
-    show_icons = true,
-    leader_key = '<leader>;', -- Recommended to be a single key
-    buffer_leader_key = '<leader>m', -- Per Buffer Mappings
-  },
-}
